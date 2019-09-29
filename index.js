a4ae04ce7d2161c3b641f0a7b39c3e655985820e
KurutsuKaren <kurutsukarenal@gmail.com>
2019-09-29 19:11:18 +0200

    Installed nedb database package, created ships.db and inserted all USS Destroyers as a test. Working fine atm.

--- index.js
+++ index.js
@@ -2,15 +2,25 @@
 
 const express = require('express');
 const app = express();
+const Datastore = require('nedb');
 
 app.listen(3000, () => console.log('Listening at 3000.'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-app.post('/api', (request, response) => {
-  console.log('Request');
-  console.log(request.body);
+app.post('/saveship', (request, response) => {
+  const ship = request.body;
+  console.log('New ship: ' + ship.name);
+  db.insert(ship);
+  
   response.json({
     response: 'success'
   });
+});
+
+const db = new Datastore('ships.db');
+db.loadDatabase((err) => {
+  if (err) {
+    console.log(err);
+  }
 });
