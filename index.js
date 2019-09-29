1dacb4b342eb11c9691a71c7bf708f4f742433d9
KurutsuKaren <kurutsukarenal@gmail.com>
2019-09-29 18:09:28 +0200

    fetch from server

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ const app = express();
 
 app.listen(3000, () => console.log('Listening at 3000.'));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
 
+app.post('/api', (request, response) => {
+  console.log('Request');
+  console.log(request.body);
+  response.json({
+    response: 'success'
+  });
+});
